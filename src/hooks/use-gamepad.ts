0c4e6644de0660596567fa95cd1e3eb5882e3951
Nathan Rajlich <n@n8.io>
2024-11-07 23:17:03 -0800

    Add `useGamepad()` hook

---
+++ src/hooks/use-gamepad.ts
@@ -0,0 +1,26 @@
+import { type DependencyList, useEffect, useRef } from "react";
+
+export function useGamepad(buttons: Record<number, () => void>, deps?: DependencyList[]) {
+    const pressed = useRef<boolean[]>([]);
+    useEffect(() => {
+        let raf: number;
+        const loop = () => {
+            const [gp] = navigator.getGamepads();
+            if (!gp) return;
+            for (const [button, onPress] of Object.entries(buttons)) {
+                const buttonNum = Number(button);
+                const wasPressed = pressed.current[buttonNum];
+                const isPressed = gp.buttons[buttonNum]?.pressed;
+                if (!wasPressed && isPressed) {
+                    pressed.current[buttonNum] = true;
+                    onPress();
+                } else if (wasPressed && !isPressed) {
+                    pressed.current[buttonNum] = false;
+                }
+            }
+            raf = requestAnimationFrame(loop);
+        }
+        loop();
+        return () => cancelAnimationFrame(raf);
+    }, deps);
+}
--- src/routes/Select.tsx
+++ src/routes/Select.tsx
@@ -1,42 +1,55 @@
 import { Button } from '@nx.js/constants';
 import { Group, Text, useRoot } from 'react-tela';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { apps } from '../apps';
 import { AppTile } from '../components/AppTile';
+import { useGamepad } from '../hooks/use-gamepad';
 
 export function Select() {
     const root = useRoot();
     const navigate = useNavigate();
     const [selectedIndex, setSelectedIndex] = useState(0);
 
-    useEffect(() => {
-        let raf: number;
-        function loop() {
-            const [gp] = navigator.getGamepads();
-            if (gp?.buttons[Button.A]?.pressed) {
-                const [path, app] = apps[selectedIndex];
-                navigate('/generate', {
-                    state: {
-                        app,
-                        path,
-                        name: app.name,
-                        author: app.author,
-                        version: app.version,
-                    },
-                });
-            } else if (gp?.buttons[Button.Right]?.pressed) {
-                setSelectedIndex(i => i + 1);
-            } else {
-                raf = requestAnimationFrame(loop);
-            }
+    const perRow = 4;
+
+    useGamepad({
+        [Button.A]() {
+            const [path, app] = apps[selectedIndex];
+            navigate('/generate', {
+                state: {
+                    app,
+                    path,
+                    name: app.name,
+                    author: app.author,
+                    version: app.version,
+                },
+            });
+        },
+        [Button.Left]() {
+            setSelectedIndex(i => {
+                if (i % perRow === 0) return i;
+                return i - 1;
+            });
+        },
+        [Button.Right]() {
+            setSelectedIndex(i => {
+                if (i === apps.length - 1) return i;
+                if (i % perRow === perRow - 1) return i;
+                return i + 1;
+            });
+        },
+        [Button.Up]() {
+            setSelectedIndex(i => Math.max(0, i - perRow));
+        },
+        [Button.Down]() {
+            setSelectedIndex(i => Math.min(apps.length - 1, i + perRow));
         }
-        raf = requestAnimationFrame(loop);
-        return () => cancelAnimationFrame(raf);
     }, [selectedIndex, navigate]);
 
     return <>
         <Text fill='white' fontSize={24}>Select an app to create a forwader for:</Text>
+        <Text fill='white' fontSize={24} x={500}>{apps[selectedIndex][0]}</Text>
         <Group y={40} width={root.ctx.canvas.width} height={root.ctx.canvas.height - 40}>
             {apps.map((app, i) => <AppTile key={app[0]} app={app[1]} index={i} selected={selectedIndex === i} />)}</Group>
     </>;
