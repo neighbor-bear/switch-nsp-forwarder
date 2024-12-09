b8c4ebfffc44beb920676b890c9f0071d84ea54a
Nathan Rajlich <n@n8.io>
2024-12-09 00:04:12 -0800

    Render cursor position caret while editing text inputs

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from 'react';
-import { Group, Rect, Text, type TextProps } from 'react-tela';
+import { useEffect, useMemo, useState } from 'react';
+import { Group, Rect, Text, useRoot, type TextProps } from 'react-tela';
 import { useGamepadButton } from '../hooks/use-gamepad';
 
 export interface TextInputProps extends TextProps {
@@ -19,9 +19,11 @@ export function TextInput({
 	y,
 	padding = 8,
 	fontSize = 24,
+	fontFamily = 'system-ui',
 	focused = false,
 	...textProps
 }: TextInputProps) {
+	const root = useRoot();
 	const [cursorPosition, setCursorPosition] = useState(-1);
 
 	useGamepadButton(
@@ -47,13 +49,24 @@ export function TextInput({
 			setCursorPosition(vk.cursorIndex);
 			onChange(vk.value);
 		}
+		function onSubmitEvent() {
+			setCursorPosition(-1);
+		}
+		vk.addEventListener('submit', onSubmitEvent);
 		vk.addEventListener('change', onChangeEvent);
 		vk.addEventListener('cursormove', onCursorMoveEvent);
 		return () => {
+			vk.removeEventListener('submit', onSubmitEvent);
 			vk.removeEventListener('change', onChangeEvent);
 			vk.removeEventListener('cursormove', onCursorMoveEvent);
 		};
 	}, [focused, onChange]);
+
+	const cursorPositionX = useMemo(() => {
+		const { ctx } = root;
+		ctx.font = `${fontSize}px ${fontFamily}`;
+		return root.ctx.measureText(value.slice(0, cursorPosition)).width;
+	}, [root, fontSize, fontFamily, cursorPosition, value]);
 
 	return (
 		<Group
@@ -63,16 +76,28 @@ export function TextInput({
 			y={y}
 			onTouchEnd={onTouchEnd}
 		>
-			<Text {...textProps} fontSize={fontSize} x={padding} y={padding}>
+			<Text
+				{...textProps}
+				fontSize={fontSize}
+				fontFamily={fontFamily}
+				x={padding}
+				y={padding}
+			>
 				{value}
 			</Text>
 			{focused && cursorPosition >= 0 ? (
-				<Rect width={1} height={fontSize} fill='white' />
+				<Rect
+					width={2}
+					height={fontSize}
+					y={padding}
+					x={cursorPositionX + padding}
+					fill='#00ffca'
+				/>
 			) : null}
 			<Rect
 				width={width + padding * 2}
 				height={fontSize + padding * 2}
-				stroke={focused ? 'white' : 'rgba(255, 255, 255, 0.5)'}
+				stroke={focused ? '#00ffca' : 'rgba(255, 255, 255, 0.5)'}
 				lineWidth={2}
 			/>
 		</Group>
